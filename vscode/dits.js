2e6a7f32250b3e101d7f8276441593f0c0c1c1c0
Yuji Tateno <tarosukenet@gmail.com>
2021-03-14 21:07:34 +0900

    gitを動かせたので

--- vscode/dits.js
+++ vscode/dits.js
@@ -1,6 +1,8 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 const vscode = require('vscode');
+const child_process = require('child_process');
+const { chdir } = require('process');
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
@@ -14,6 +16,16 @@ function activate(context) {
 	vscode.window.showInformationMessage('Hello World!');
 
 
+	//gitの起動と出力の回収
+//	const proc = child_process.spawn('git', ['commit', '-a', '-m', 'gitを動かせたので'], chdir('dits'));
+	const proc = child_process.spawn('git', ['status']);
+	console.log("child:" + proc.pid);
+	proc.stdout.on('data', (data) => {
+		console.log(data.toString());
+	});
+	proc.stderr.on('data', (data) => {
+		console.error(data.toString());
+	});
 
 
 }
