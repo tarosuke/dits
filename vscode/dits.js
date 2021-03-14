ba8f20863e749119bbe7f3ec034fbf15f255b1d9
Yuji Tateno <tarosukenet@gmail.com>
2021-03-14 11:13:46 +0900

    ビューコンテナを追加

--- vscode/dits.js
+++ vscode/dits.js
@@ -9,21 +9,13 @@ const vscode = require('vscode');
  * @param {vscode.ExtensionContext} context
  */
 function activate(context) {
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
 	console.log('Congratulations, your extension "helloworld-minimal-sample" is now active!');
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with  registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('extension.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
+	vscode.window.showInformationMessage('Hello World!');
 
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World!');
-	});
 
-	context.subscriptions.push(disposable);
+
+
 }
 
 // this method is called when your extension is deactivated
