f1b5064fc6239c81668d97986da8579a0be6afb0
Yuji Tateno <tarosukenet@gmail.com>
2021-03-20 17:12:35 +0900

    履歴のコメントとコマンドを分離

--- vscode/repository.js
+++ vscode/repository.js
@@ -10,23 +10,25 @@ exports.Repository = function () {
 	var out = Git(['log', '--oneline', '--no-decorate'], 'dits');
 	if (out) {
 		const outStr = out.slice(1, -1);
-		console.log(outStr);
-		for (const item of outStr.split('\n')) {
-			if (item.trim().length) {
+		// console.log(outStr);
+		for (var item of outStr.split('\n')) {
+			item = item.trim();
+			if (item.length) {
 				//各コミット分
 				const commit = {
 					hash: item.slice(1, 7).trim(),
 					label: item.slice(8).trim(),
 					collapsibleState: null
 				};
-
-
-
-				this.items.push(commit);
+				if (commit.label.indexOf('.dits') < 0) {
+					//コメント
+					this.items.push(commit);
+				} else {
+					//コマンド
+					vscode.window.log(commit.label);
+				}
 			}
 		}
-	} else {
-		vscode.window.showErrorMessage(out.stderr.toString());
 	}
 };
 
