5d9ccb70632457f57840d023032fa60430f75156
Yuji Tateno <tarosukenet@gmail.com>
2021-03-28 22:06:28 +0900

    リリース用にパスを修正

--- vscode/dits.js
+++ vscode/dits.js
@@ -13,8 +13,7 @@ const { Repository } = require('./repository.js');
  * @param {vscode.ExtensionContext} context
  */
 function activate(context) {
-	// this.currentPath = vscode.workspace.workspaceFolders;
-	this.currentPath = 'dits';
+	this.currentPath = vscode.workspace.workspaceFolders[0];
 
 	//リポジトリの取得
 	this.repository = new Repository(currentPath);
